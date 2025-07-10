f6e05df39e217828f040fdba197b1a03ce2c0d94
Isaac Njenga <ayzzoh20@gmail.com>
2025-07-10 18:14:14 +0300

    Implement authentication screens with login and signup functionality; add SafeScreen component for layout management

---
+++ front/app/(auth)/signup.jsx
@@ -0,0 +1,141 @@
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import styles from "@/assets/styles/signup.styles.js";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "@/constants/colors";
+import { Link, useRouter } from "expo-router";
+
+const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSignUp = async () => {};
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <View style={styles.card}>
+          {/* header */}
+          <View style={styles.header}>
+            <Text style={styles.title}>BookWorm🐛</Text>
+            <Text style={styles.subtitle}>Share your favourite reads</Text>
+          </View>
+
+          <View style={styles.formContainer}>
+            {/* username */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Username</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="person-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your username"
+                  placeholderTextColor="#888"
+                  autoCapitalize="none"
+                  value={username}
+                  onChangeText={setUsername}
+                />
+              </View>
+            </View>
+          </View>
+
+          {/* email*/}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}>Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="john@email.com"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+          </View>
+
+          {/* password */}
+          <View>
+            <Text style={styles.label}>Password</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="lock-closed-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="*********"
+                placeholderTextColor={COLORS.placeholderText}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+              />
+
+              <TouchableOpacity
+                onPress={() => setShowPassword(!showPassword)}
+                style={styles.eyeIcon}
+              >
+                <Ionicons
+                  name={showPassword ? "eye-off-outline" : "eye-outline"}
+                  size={20}
+                  color={COLORS.primary}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={handleSignUp}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Text style={styles.buttonText}>Sign Up</Text>
+            )}
+          </TouchableOpacity>
+
+          {/* footer */}
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>Already have an account?</Text>
+            <TouchableOpacity onPress={() => router.back()}>
+              <Text style={styles.link}>Sign In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Signup;
--- front/app/index.jsx
+++ front/app/index.jsx
@@ -1,15 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello</Text>
-      <Image
-        source={{
-          uri: "https://images.unsplash.com/photo-1526554850534-7c78330d5f90?w=800&auto=format&fit=crop&q=60",
-        }}
-      />
+      <Link href="/(auth)/signup">Sign Up</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
